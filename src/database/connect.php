aa4f81e58d8a75eafc48aaf9e72dba6161a42ac1
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-05 17:46:41 +0100

    fuck you github

---
+++ src/database/connect.php
@@ -0,0 +1,14 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+
+$servername = DB_SERVER;
+$dbname = DB_NAME;
+$username = DB_USERNAME;
+$password = DB_PASSWORD;
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+if ($connection->connect_error) {
+  die('Connection failed: ' . $connection->connect_error);
+}
+?>
