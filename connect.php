c25b62a7e3f89e1e3c0fa7e822dd6dc13306028a
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-11 15:01:49 +0100

    gf

--- connect.php
+++ connect.php
@@ -6,6 +6,7 @@ $password = "es,RV.J3&7Bg'U=";
 // Create connection using musqli_connect function
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Connection Check
+// IGNORE RED ERROR LINE THAT IS THERE FOR SOME REASON IDK HOW TO GET RID OF IT
 if (!$conn) {
     die("Connection failed: " . $conn->connect_error);
 }else{
