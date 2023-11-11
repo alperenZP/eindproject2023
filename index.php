73b5cc378839b474fee18efbfaa001d0d6a8dd1c
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-11 14:36:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
+</body>
+</html>
+
 <?php
 
 echo "Hello woooooolrld!";
