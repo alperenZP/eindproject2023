be3d45008ef16599df2988940e65e54f587566e8
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-11 17:00:45 +0100

    added connect.php
    time to get things started i guess

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
+    require "connect.php";
     echo "This is a simple Webpage"."<br><br>";
-
-    $servername = "localhost";
-    
-    // Database Variables
-    $dbname     = "biblwnot_database";
-    $username   = "biblwnot_roman4";
-    $password   = "es,RV.J3&7Bg'U=";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Checking Connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
     // SQL query to fetch data
     $sql = "SELECT * FROM users";
