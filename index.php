625a7900aa07f02e0d5ed2cbf9b8e9cb99f82ec7
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-12 21:02:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+    require "connect.php";
+    echo "This is a simple Webpege" . "<br><br>";
+
+    // SQL query to fetch data
+    $sql = "SELECT * FROM users";
+
+    $result = $conn->query($sql);
+
+    if ($result = $conn->query($sql)) {
+        while ($row = $result->fetch_assoc()) {
+            echo $row['id'] . " ";
+            echo $row['username'] . " ";
+            echo $row['password'] . " ";
+        }
+    } else {
+        echo "Error:" . $sql . "<br>" . $conn->error;
+    }
+
+    // Closing the connection
+    $conn->close();
+?>
+
 <!doctype html>
 <html>
 
@@ -66,26 +89,3 @@
 </body>
 
 </html>
-
-<?php
-require "connect.php";
-echo "This is a simple Webpege" . "<br><br>";
-
-// SQL query to fetch data
-$sql = "SELECT * FROM users";
-
-$result = $conn->query($sql);
-
-if ($result = $conn->query($sql)) {
-    while ($row = $result->fetch_assoc()) {
-        echo $row['id'] . " ";
-        echo $row['username'] . " ";
-        echo $row['password'] . " ";
-    }
-} else {
-    echo "Error:" . $sql . "<br>" . $conn->error;
-}
-
-// Closing the connection
-$conn->close();
-?>
