3d687a3567b94718500eccf20f44143af5b6b6e7
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-12 15:28:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $conn->close();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/dist/output.css" rel="stylesheet">
+  <link href="./dist/output.css" rel="stylesheet">
 </head>
 <body>
   <h1 class="text-3xl font-bold underline">
