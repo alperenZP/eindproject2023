d5e3a4b15f6101d13c5a65e831cec26e418c94a6
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-15 23:11:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $conn->close();
   <link rel="shortcut icon" href="./assets/img/favicon.ico" />
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/tailwind.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css" />
   <title>Bibliotheek.Live</title>
 </head>
 
