630827d66b07e0cdb618a9fc5c186e254d4d6c5d
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-15 21:46:50 +0100

    ok here we go!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: [
+    "./src/**/*.{html,js,php}",
+    "./index.php"
+  ],
   theme: {
     extend: {},
   },
