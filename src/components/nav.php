5b2da808630dbdbec5b38ca1a2d83d6089e2015e
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-17 21:22:55 +0100

    Update nav.php

--- src/components/nav.php
+++ src/components/nav.php
@@ -47,7 +47,7 @@ $searchTerm = $_GET['search'] ?? '';
             </details>
           </li>
           '
-          : '<li><a href="alperenGit/account/login" class="text-neutral-content">Lgin</a></li>'
+          : '<li><a href="alperenGit/account/login" class="text-neutral-content">HUGHGERJKJGH</a></li>'
         ?>
         <!-- Language Select -->
         <li>
