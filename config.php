34e1ec09cd1a4b33beeac2f5b2cc9b3935d16642
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-17 20:11:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 //define('DB_USERNAME', 'root');
 //define('DB_PASSWORD', "");
 //define('DB_NAME', 'bibliotheek');
-define('DB_SERVER', ' localhost');
+define('DB_SERVER', 'server203');
 define('DB_USERNAME', 'biblwnot_roman4');
 define('DB_PASSWORD', "es,RV.J3&7Bg'U=");
 define('DB_NAME', 'biblwnot_database');
