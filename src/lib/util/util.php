f6660b9939deb4ac638b9a05c16a0cb851a6ab20
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-17 20:21:58 +0100

    Update util.php

--- src/lib/util/util.php
+++ src/lib/util/util.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
 require_once DATABASE . '/connect.php';
 
 function fetch($query, ...$params) {
