363040bf7496d288745e2b39d37f92d91cc1fa4c
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-19 16:31:51 +0100

    Update login.php

--- src/lib/account/login.php
+++ src/lib/account/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
 require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
