8f4cb91f058c90b794efdbba7d42bb840f488a58
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-19 14:44:56 +0100

    Update login.php

--- src/public/account/login.php
+++ src/public/account/login.php
@@ -49,6 +49,6 @@ if (isset($_SESSION['user'])) {
   </form>
 
   <div class="w-full text-center mt-8">
-    <a class="link" href="./register.php">I don't have an account yet</a>
+    <a class="link" href="./register.php">Register</a>
   </div>
 </div>
