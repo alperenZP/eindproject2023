1f0ca9785afbd836b4862ee5b891dc28cf5a3e2c
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-19 16:58:54 +0100

    Update nav.php

--- src/components/nav.php
+++ src/components/nav.php
@@ -62,7 +62,7 @@ $searchTerm = $_GET['search'] ?? '';
       <details class="dropdown dropdown-end">
         <summary class="m-1 btn btn-secondary btn-circle avatar">
           <div class="w-10 rounded-full">
-            <img src="https://commons.wikimedia.org/wiki/File:Profile_photo_placeholder_square.svg" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Profile_photo_placeholder_square.svg" />
           </div>
         </summary>
         <ul class="mt-2 p-2 shadow menu dropdown-content z-[1] bg-base-200 rounded-box w-52">
