bd1427f1a86194571df73d7254ed7ae4e78660c0
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-21 17:47:44 +0100

    Update create_book.php

--- src/public/teacher/create_book.php
+++ src/public/teacher/create_book.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['user'])) {
     exit();
 } 
 
-if ($_SESSION["user"]["isTeacher"]) {
+if (!$_SESSION["user"]["isTeacher"]) {
     header('Location: https://bibliotheek.live');
     exit();
 } 
