bc741cfa0b02a70c256713e56a91253859789ae8
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-21 19:05:08 +0100

    Update create_book.php

--- src/lib/user/teacher/create_book.php
+++ src/lib/user/teacher/create_book.php
@@ -5,33 +5,33 @@ require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
 if (!isset($_SESSION['user'])) {
-  header('Location: /');
-  exit();
+    header('Location: /');
+    exit();
 }
 
 if (!$_SESSION["user"]["isTeacher"]) {
     header('Location: https://bibliotheek.live');
     exit();
-} 
+}
 
 if (isset($_POST['create'])) {
-  $creatorid = $_SESSION["user"]["id"];
-  $subjectid = $_POST['bookSubject'];
-  $title = $_POST['title'];
-  $description = $_POST['description'];
-  $accessCode = uniqid();
+    $creatorid = $_SESSION["user"]["id"];
+    $subjectid = $_POST['bookSubject'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $accessCode = uniqid();
 
-  $query = 'INSERT INTO books (creatorid, title, subjectid, description, accessCode) VALUES (?, ?, ?, ?, ?)';
-  insert(
-    $query,
-    ['type' => 'i', 'value' => $creatorid],
-    ['type' => 's', 'value' => ''.$title.''],
-    ['type' => 'i', 'value' => $subjectid],
-    ['type' => 's', 'value' => ''.$description.''],
-    ['type' => 's', 'value' => ''.$accessCode.''],
-  );  
-  header('Location: https://bibliotheek.live');
-  return;
+    $query = 'INSERT INTO books (creatorid, title, subjectid, description, accessCode) VALUES (?, ?, ?, ?, ?)';
+    insert(
+        $query,
+        ['type' => 'i', 'value' => $creatorid],
+        ['type' => 's', 'value' => '' . $title . ''],
+        ['type' => 'i', 'value' => $subjectid],
+        ['type' => 's', 'value' => '' . $description . ''],
+        ['type' => 's', 'value' => '' . $accessCode . ''],
+    );
+    header('Location: https://bibliotheek.live');
+    exit();
 }
 
 header('Location: https://bibliotheek.live');
