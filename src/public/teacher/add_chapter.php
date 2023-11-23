e74aedbaa85771c7c6d6903313456e95a8e3419d
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-23 19:03:28 +0100

    Update add_chapter.php

--- src/public/teacher/add_chapter.php
+++ src/public/teacher/add_chapter.php
@@ -46,6 +46,7 @@ $books = fetch('SELECT * FROM books WHERE creatorid = ' . $_SESSION["user"]["id"
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span>Het hoofdstuk is met succes aan het boek toegevoegd</span>
             </div>
+            <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8"> </h1>
         ';
     }
     ?>
