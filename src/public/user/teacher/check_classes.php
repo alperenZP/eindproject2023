4ca57a7013c57664a41747509ef40566ce672ae2
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 19:50:21 +0100

    added check classes page

---
+++ src/public/user/teacher/check_classes.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header('Location: https://bibliotheek.live');
+    exit();
+}
+if (!$_SESSION["user"]["isTeacher"]) {
+    header('Location: https://bibliotheek.live');
+    exit();
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
+require_once DATABASE . '/connect.php';
+require_once LIB . '/util/util.php';
+
+$books = fetch_as_array('SELECT * FROM `books` WHERE creatorid = '.$_SESSION["user"]["id"].'');
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script src="https://kit.fontawesome.com/58a210823e.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="/alperenGit/public/css/theme.css">
+    <title></title>
+</head>
+<?php include COMPONENTS . '/nav.php' ?>
+<div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
+    <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Controleer wie er aan uw boeken is gekoppeld</h1>
+    <div class="dropdown">
+            <div tabindex="0" role="button" class="btn m-1">Sorteer op boek</div>
+            <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                <?php
+                    foreach ($books as $book) {
+                        echo '
+                            <li><a href="https://bibliotheek.live/alperenGit/src/public/user/check_students.php?bookid='.$book["id"].'">'.$book["title"].'</a></li>
+                        ';
+                    }
+                ?>
+            </ul>
+        </div>
+
+    <?php
+        if (count($books) == 0){
+            echo '
+                <div role="alert" class="alert alert-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>Er staan geen boeken in deze lijst.</span>
+                </div>
+            ';
+        }
+    ?>
+</div>
---
+++ src/public/user/teacher/check_students.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header('Location: https://bibliotheek.live');
+    exit();
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
+require_once DATABASE . '/connect.php';
+require_once LIB . '/util/util.php';
+
+
+$users = fetch_as_array('SELECT * FROM `users` INNER JOIN book_connections ON (book_connections.userid = users.id) INNER JOIN books ON (books.id = book_connections.bookid) WHERE bookid = ? AND books.creatorid = ?',
+    ['type' => 'i', 'value' => $_GET["bookid"]],
+    ['type' => 'i', 'value' => $_SESSION["user"]["id"]],
+);
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script src="https://kit.fontawesome.com/58a210823e.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="/alperenGit/public/css/theme.css">
+    <title>Bibliotheek</title>
+</head>
+<?php include COMPONENTS . '/nav.php' ?>
+<div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
+    <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Gebruikers die zijn verbonden met dit boek</h1>
+
+    <div class="overflow-x-auto">
+        <table class="table table-zebra">
+            <tbody>
+                <tr>
+                    <td>Voornaam</td>
+                    <td>Naam</td>
+                    <td>Beschrijving</td>
+                </tr>
+                <!-- row -->
+                <?php
+                foreach ($books as $book) {
+                    echo '
+                        <tr>
+                            <td><img src="' . $book["image_link"] . '" height="50px" width="50px"></td>
+                            <td><a href="https://www.example.com"><u>' . $book["title"] . '</u></a></td>
+                            <td>' . $book["description"] . '</td>
+                        </tr>        
+                    ';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    
+    <?php
+        if (count($books) == 0){
+            echo '
+                <div role="alert" class="alert alert-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>Er staan geen boeken in deze lijst.</span>
+                </div>
+            ';
+        }
+    ?>
+</div>
