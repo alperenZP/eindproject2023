fffd3d8afb430b1c32cd101679524f0389e580eb
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 19:20:15 +0100

    Update view_library.php

--- src/public/user/view_library.php
+++ src/public/user/view_library.php
@@ -49,7 +49,6 @@ $subjects = fetch('SELECT * FROM book_subjects');
         </div>
 
     <div class="overflow-x-auto">
-        
         <table class="table table-zebra">
             <tbody>
                 <tr>
@@ -72,4 +71,15 @@ $subjects = fetch('SELECT * FROM book_subjects');
             </tbody>
         </table>
     </div>
+    
+    <?php
+        if (count($books) == 0){
+            echo '
+                <div role="alert" class="alert alert-info">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>Er staan geen boeken in deze lijst.</span>
+                </div>
+            ';
+        }
+    ?>
 </div>
