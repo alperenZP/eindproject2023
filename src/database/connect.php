83c870ee51b9f9b71f45d1460d6c5fa83c8349ef
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 14:58:01 +0100

    Update connect.php

--- src/database/connect.php
+++ src/database/connect.php
@@ -1,6 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
-
+header('Content-type: text/plain; charset=utf-8'); 
 $servername = DB_SERVER;
 $dbname = DB_NAME;
 $username = DB_USERNAME;
