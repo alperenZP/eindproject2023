08b6d3f4e3b3e6742fd898f8ad5d6fad7624ee4f
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 14:41:23 +0100

    Update view_library.php

--- src/public/user/view_library.php
+++ src/public/user/view_library.php
@@ -30,22 +30,23 @@ $books = fetch("SELECT * FROM books");
 <?php include COMPONENTS . '/nav.php' ?>
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Jouw boeken</h1>
-    <?php
-    foreach ($books as $book) {
-        echo '
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="card-title">'.$book["title"].'</h2>
-                    <p>'.$book["description"].'</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Bekijk</button>
+    <div class="join join-vertical lg:join-horizontal">
+        <?php
+        foreach ($books as $book) {
+            echo '
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <div class="card-body">
+                        <h2 class="card-title">'.$book["title"].'</h2>
+                        <p>'.$book["description"].'</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Bekijk</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ';
-    }
+            ';
+        }
+        ?>
+    </div>
 
-
-    ?>
 
 </div>
