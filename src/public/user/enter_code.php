c4b245ecff17aa60f5d94968a5f1e212e24b2b32
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 17:05:32 +0100

    Update enter_code.php

--- src/public/user/enter_code.php
+++ src/public/user/enter_code.php
@@ -28,6 +28,38 @@ require_once LIB . '/util/util.php';
 <?php include COMPONENTS . '/nav.php' ?>
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Geef code van boek in</h1>
+    <?php
+    if (isset($_GET["failure"])) {
+        echo '
+            <div class="divider divider-neutral"></div>
+        ';
+        if ($_GET["failure"] == "ac") {
+            echo '
+                <div role="alert" class="alert alert-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>U bent al verbonden met dit boek.</span>
+                </div>
+            ';
+        } elseif ($_GET["failure"] == "de") {
+            echo '
+                <div role="alert" class="alert alert-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                    <span>De code die u heeft ingevoerd is ongeldig.</span>
+                </div>
+            ';
+        }
+        echo '
+            <div class="divider divider-neutral"></div>
+        ';
+    } elseif (isset($_GET["bookid"])){
+        echo '
+            <div role="alert" class="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>U bent succesvol verbonden. Controleer uw bibliotheek om dit boek te bekijken.</span>
+            </div>
+        ';
+    }
+    ?>
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8"> </h1>
     <form action="https://bibliotheek.live/alperenGit/src/lib/user/enter_code.php" method="post" class="flex flex-col gap-8 w-full sm:w-80">
         <div class="flex flex-col gap-4">
