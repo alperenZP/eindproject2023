9ca52eefd38e7b55de9bc5efbb59edb0dcbd2718
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-11-26 19:08:08 +0100

    Update view_library.php

--- src/public/user/view_library.php
+++ src/public/user/view_library.php
@@ -10,7 +10,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
 require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
-$books = fetch_as_array('SELECT * FROM `books` INNER JOIN book_subjects ON (books.subjectid = book_subjects.id) INNER JOIN book_connections ON (books.id = book_connections.bookid) WHERE book_connections.userid ='.$_SESSION["user"]["id"].'')
+if (isset($_GET["subject"])){
+    $book_query = "AND book_subjects.id = " . $_GET["subject"];
+} else {
+    $book_query = "";
+}
+$books = fetch_as_array('SELECT * FROM `books` INNER JOIN book_subjects ON (books.subjectid = book_subjects.id) INNER JOIN book_connections ON (books.id = book_connections.bookid) WHERE book_connections.userid =' . $_SESSION["user"]["id"] . ' ' . $book_query);
 
 ?>
 
@@ -32,6 +37,13 @@ $books = fetch_as_array('SELECT * FROM `books` INNER JOIN book_subjects ON (book
 
 
     <div class="overflow-x-auto">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn m-1">Click</div>
+            <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+            </ul>
+        </div>
         <table class="table table-zebra">
             <tbody>
                 <tr>
@@ -45,8 +57,8 @@ $books = fetch_as_array('SELECT * FROM `books` INNER JOIN book_subjects ON (book
                     echo '
                         <tr>
                             <td><img src="' . $book["image_link"] . '" height="50px" width="50px"></td>
-                            <td><a href="https://www.example.com"><b>' . $book["title"] . '</b></a></td>
-                            <td>'.$book["description"].'</td>
+                            <td><a href="https://www.example.com"><u>' . $book["title"] . '</u></a></td>
+                            <td>' . $book["description"] . '</td>
                         </tr>        
                     ';
                 }
