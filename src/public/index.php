0186baa5914948ac04a6b8c53c79410ddd15d2de
Aalleepphh <alperen.tok@bazandpoort.be>
2023-12-01 11:00:51 +0100

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+  session_start();
 ?>
 <div class="hero min-h-screen" style="background-image: url(https://upload.wikimedia.org/wikipedia/commons/f/f0/Library_Books_Bookshelves_%28Unsplash%29.jpg);">
   <div class="hero-overlay bg-opacity-60"></div>
@@ -7,7 +7,7 @@
     <div class="max-w-md">
       <h1 class="mb-5 text-5xl font-bold">Welkom</h1>
       <p class="mb-5">Dit is de Bibliotheek Live, de levende bibliotheek.</p>
-      <button class="btn btn-primary">Begin je reis</button>
+      <a href="<?php if (isset($_SESSION["user"])){echo 'https://bibliotheek.live/alperenGit/src/public/user/enter_code.php';}else{echo 'https://bibliotheek.live/alperenGit/src/public/account/login.php';}?>"><button class="btn btn-primary">Begin je reis</button></a>
     </div>
   </div>
 </div>
