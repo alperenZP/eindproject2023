801f96b39adc6ebc4ba153fdc07f1487ef77f36f
Aalleepphh <alperen.tok@bazandpoort.be>
2024-01-08 15:20:44 +0100

    Update view_book.php

--- src/public/user/view_book.php
+++ src/public/user/view_book.php
@@ -60,7 +60,7 @@ $chapters = fetch_as_array(
                         <tr>
                             <td>' . $chapterIndex . '</td>
                             <td>' . $chapter["chapterTitle"] . '</td>
-                            <td><a title="PDF" href="https://bibliotheek.live/alperenGit/public/pdf/bid'.$chapter["bookid"].'_hid'.$chapter["chapterid"].'"><img width="32" alt="PDF icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/PDF_icon.svg/32px-PDF_icon.svg.png"></a></td>
+                            <td><a title="PDF" href="https://bibliotheek.live/alperenGit/public/pdf/bid'.$chapter["bookid"].'_hid'.$chapter["chapterid"].'.pdf"><img width="32" alt="PDF icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/PDF_icon.svg/32px-PDF_icon.svg.png"></a></td>
                             <td><a href="https://www.example.com"><u>Forum</u></a></td>
                         </tr>        
                     ';
