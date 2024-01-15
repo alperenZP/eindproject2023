eb99466c706796b43fd732df92fc06386c9a4c0c
Aalleepphh <alperen.tok@bazandpoort.be>
2024-01-15 16:08:08 +0100

    Update edit_chapter.php

--- src/public/user/teacher/edit_chapter.php
+++ src/public/user/teacher/edit_chapter.php
@@ -32,17 +32,6 @@ $chapter = fetch('SELECT * FROM book_chapters WHERE id = ?', ['type' => 'i', 'va
 </head>
 <?php include COMPONENTS . '/nav.php' ?>
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
-    <?php
-    if (isset($_GET["bookid"])) {
-        echo '
-            <div role="alert" class="alert alert-success">
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>Het hoofdstuk is met succes aan het boek toegevoegd. Klik <a href="https://bibliotheek.live/alperenGit/src/public/user/teacher/share_invite.php?bookid=' . $_GET["bookid"] . '"><b>hier</b></a> om uw boek te delen.</span>
-            </div>
-            <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8"> </h1>
-        ';
-    }
-    ?>
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Wijzig hoofdstuk</h1>
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8"> </h1>
     <form action="https://bibliotheek.live/alperenGit/src/lib/user/teacher/edit_chapter.php" method="post"
