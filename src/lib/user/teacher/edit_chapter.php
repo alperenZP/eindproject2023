7abd6a5eb7700c723ce06c2fa128c28b50ea8669
Aalleepphh <alperen.tok@bazandpoort.be>
2024-01-19 15:06:20 +0100

    effe

--- src/lib/user/teacher/edit_chapter.php
+++ src/lib/user/teacher/edit_chapter.php
@@ -4,16 +4,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
 require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
-if (!isset($_SESSION['user'])) {
-    header('Location: /');
-    exit();
-}
-
-if (!$_SESSION["user"]["isTeacher"]) {
-    header('Location: https://bibliotheek.live');
-    exit();
-}
-
 if (isset($_POST['edit'])) {
     $bookid = $_POST['bookid'];
     $title = $_POST['title'];
