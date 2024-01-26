927e28b42e79a612a59407e47fc8f025be97b219
alperenZP <144694586+alperenZP@users.noreply.github.com>
2024-01-26 15:11:46 +0100

    Update chat.php

--- src/public/user/chat/chat.php
+++ src/public/user/chat/chat.php
@@ -1,30 +1,15 @@
 <?php
-session_start();
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
-require_once DATABASE . '/connect.php';
-require_once LIB . '/util/util.php';
-$theme = 'dark';
-
+    session_start();
 ?>
 
 <!DOCTYPE html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.3/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com"></script>
-
-    <script src="https://kit.fontawesome.com/58a210823e.js" crossorigin="anonymous"></script>
-
-    <link rel="stylesheet" href="/alperenGit/public/css/theme.css">
-    <title>Stuur vraag</title>
+    <title>Chat</title>
 </head>
-<?php include COMPONENTS . '/nav.php' ?>
-<div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
-    <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Chat</h1>
+<body>
+    <h1>Chat</h1>
     <?php
         include 'shoutbox.inc.php';
     ?>
-</div>
+</body>
