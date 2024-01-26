eca3f2fd88c11ac29c495e82d03352be99d12cc2
alperenZP <144694586+alperenZP@users.noreply.github.com>
2024-01-26 15:03:15 +0100

    Update chat.php

--- src/public/user/chat/chat.php
+++ src/public/user/chat/chat.php
@@ -21,7 +21,6 @@ $theme = 'dark';
     <link rel="stylesheet" href="/alperenGit/public/css/theme.css">
     <title>Stuur vraag</title>
 </head>
-<?php include COMPONENTS . '/nav.php' ?>
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Chat box</h1>
     <?php
