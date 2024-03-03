294a7e91de7c44320ea2f3969ee0fdad7991d6b5
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-03-03 12:06:36 +0100

    Update chat.php

--- src/public/user/chat/chat.php
+++ src/public/user/chat/chat.php
@@ -51,7 +51,7 @@ if ($book_access["amount"] == 0 && $book_creator["amount"] == 0) {
             <h2 class="card-title">Vraag:</h2>
             <p><?php echo $lobby["question"]; ?></p>
         </div>
-        <figure><img src="https://bibliotheek.live/alperenGit/public/img/<?php echo $lobby["img_code"]?>.webp" alt="Shoes" /></figure>
+        <figure><img height="50px" src="https://bibliotheek.live/alperenGit/public/img/<?php echo $lobby["img_code"]?>.webp" alt="Shoes" /></figure>
     </div>
     <?php
     $_SESSION["lobbyid"] = $lobby["id"];
