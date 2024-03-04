9acbf87f0fcedfc56f97f2ba67658461c7b8d412
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-03-04 01:22:35 +0100

    Update notifications.php

--- src/public/user/notifications.php
+++ src/public/user/notifications.php
@@ -19,7 +19,7 @@ $books = fetch_as_array('SELECT *, books.id AS "bookid" FROM `books` INNER JOIN 
 $subjects = fetch('SELECT * FROM book_subjects');
 $theme = 'dark';
 
-$created_books = fetch_as_array('SELECT * FROM books INNER JOIN book_subjects ON (books.subjectid = book_subjects.id) INNER JOIN book_connections ON (books.id = book_connections.bookid) WHERE books.creatorid = ' . $_SESSION["user"]["id"] . ' '.$book_query.' GROUP BY books.id');
+$created_books = fetch_as_array('SELECT * FROM books INNER JOIN book_subjects ON (books.subjectid = book_subjects.id) INNER JOIN book_connections ON (books.id = book_connections.bookid) WHERE books.creatorid = ' . $_SESSION["user"]["id"] . ' ' . $book_query . ' GROUP BY books.id');
 
 ?>
 
@@ -33,69 +33,32 @@ $created_books = fetch_as_array('SELECT * FROM books INNER JOIN book_subjects ON
     <script src="https://kit.fontawesome.com/58a210823e.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="/alperenGit/public/css/theme.css">
-    <title>Bibliotheek</title>
+    <title>Notificaties</title>
 </head>
 <?php include COMPONENTS . '/nav.php' ?>
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
-    <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Bibliotheek</h1>
-
-    <div class="dropdown">
-        <div tabindex="0" role="button" class="btn m-1">Sorteer op onderwerp</div>
-        <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a href="https://bibliotheek.live/alperenGit/src/public/user/view_library.php">Alles</a></li>
-            <?php
-            foreach ($subjects as $subject) {
-                echo '
-                            <li><a href="https://bibliotheek.live/alperenGit/src/public/user/view_library.php?subject=' . $subject["id"] . '">' . $subject["name"] . '</a></li>
-                        ';
-            }
-            ?>
-        </ul>
-    </div>
+    <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Notificaties</h1>
 
     <div class="overflow-x-auto">
         <table class="table table-zebra">
-            <tbody>
+            <!-- head -->
+            <thead>
                 <tr>
-                    <td>Onderwerp</td>
-                    <td>Titel</td>
-                    <td>Beschrijving</td>
+                    <th>Van</th>
+                    <th>Boek</th>
+                    <th>Onderwerp</th>
                 </tr>
-                <!-- row -->
-
-                <?php
-                foreach ($created_books as $book) {
-                    echo '
-                        <tr>
-                            <td><img src="' . $book["image_link"] . '" height="50px" width="50px"></td>
-                            <td><a href="https://bibliotheek.live/alperenGit/src/public/user/view_book.php?book=' . $book["bookid"] . '"><u>'. $book["books.id"] . $book["title"] . '</u></a></td>
-                            <td>' . $book["description"] . '</td>
-                        </tr>        
-                    ';
-                }
-
-                foreach ($books as $book) {
-                    echo '
-                        <tr>
-                            <td><img src="' . $book["image_link"] . '" height="50px" width="50px"></td>
-                            <td><a href="https://bibliotheek.live/alperenGit/src/public/user/view_book.php?book=' . $book["bookid"] . '"><u>'. $book["books.id"] . $book["title"] . '</u></a></td>
-                            <td>' . $book["description"] . '</td>
-                        </tr>        
-                    ';
-                }
-                ?>
+            </thead>
+            
+            <tbody>
+                <!-- row 1 -->
+                <tr>
+                    <th>1</th>
+                    <td>Cy Ganderton</td>
+                    <td>Quality Control Specialist</td>
+                    <td>Blue</td>
+                </tr>
             </tbody>
         </table>
     </div>
-
-    <?php
-    if (count($books) == 0) {
-        echo '
-                <div role="alert" class="alert alert-info">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span>Er staan geen boeken in deze lijst.</span>
-                </div>
-            ';
-    }
-    ?>
 </div>
