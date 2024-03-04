fe806fe3a737ccb91465df409c53dfb13b994dba
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-03-04 19:52:20 +0100

    Update chat.php

--- src/public/user/chat/chat.php
+++ src/public/user/chat/chat.php
@@ -42,7 +42,7 @@ if ($lobby["senderid"] != $_SESSION['user']['id'] && !$_SESSION["user"]["isTeach
 }
 
 $visits = fetch_as_array(
-    'SELECT COUNT(*) AS "amount" FROM visits (visitorid, lobbyid) VALUES (?, ?)',
+    'SELECT *, COUNT(*) AS "amount" FROM visits (visitorid, lobbyid) VALUES (?, ?)',
     ['type' => 'i', 'value' => $_SESSION["user"]["id"]],
     ['type' => 'i', 'value' => $lobby["id"]],
 );
