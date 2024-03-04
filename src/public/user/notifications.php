7712126826dbf24f29de7eb456eacb0e790114b4
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-03-04 20:59:22 +0100

    Update notifications.php

--- src/public/user/notifications.php
+++ src/public/user/notifications.php
@@ -24,6 +24,14 @@ if ($_SESSION["user"]["isTeacher"]){
     INNER JOIN visits ON (lobbies.id = visits.lobbyid AND visits.visitorid = books.creatorid)
     WHERE visits.timestamp < Shoutbox.Timestamp AND books.creatorid = '.$_SESSION["user"]["id"].'
     GROUP BY Shoutbox.Lobbyid');
+} else {
+    
+    $notifications = fetch_as_array('SELECT *, COUNT(*) AS "pings" FROM `Shoutbox` 
+    INNER JOIN lobbies ON (lobbies.id = Shoutbox.Lobbyid)
+    INNER JOIN books ON (books.id = lobbies.bookid)
+    INNER JOIN visits ON (lobbies.id = visits.lobbyid AND visits.visitorid = '.$_SESSION["user"]["id"].')
+    WHERE visits.timestamp < Shoutbox.Timestamp AND Shoutbox.Senderid != '.$_SESSION["user"]["id"].' AND lobbies.senderid = '.$_SESSION["user"]["id"].'
+    GROUP BY Shoutbox.Lobbyid');
 }
 ?>
 
