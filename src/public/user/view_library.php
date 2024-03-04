b1cb4221ad962b831604026d15f1eed5523719df
Aalleepphh <alperen.tok@bazandpoort.be>
2024-03-04 15:43:14 +0100

    Update view_library.php

--- src/public/user/view_library.php
+++ src/public/user/view_library.php
@@ -89,7 +89,7 @@ $created_books = fetch_as_array('SELECT * FROM books INNER JOIN book_subjects ON
     </div>
 
     <?php
-    if (count($books) == 0) {
+    if (count($books) == 0 && count($created_books) == 0) {
         echo '
                 <div role="alert" class="alert alert-info">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
