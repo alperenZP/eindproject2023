0848a4afbd1ca506907f55043dc800218bcb9694
Aalleepphh <alperen.tok@bazandpoort.be>
2024-03-08 14:28:25 +0100

    Update view_book.php

--- src/public/user/view_book.php
+++ src/public/user/view_book.php
@@ -57,7 +57,7 @@ $chapters = fetch_as_array(
         ?>
     </h1>
 
-    <ul class="menu menu-horizontal bg-base-200 w-100">
+    <ul class="menu menu-horizontal bg-base-200 w-200">
         <li><a>Item 1</a></li>
         <li><a class="active">Item 2</a></li>
         <li><a>Item 3</a></li>
