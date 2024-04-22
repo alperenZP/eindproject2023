ef42d336e7a605ddb0279720da71848c6fc7a47a
Aalleepphh <alperen.tok@bazandpoort.be>
2024-04-22 10:06:09 +0200

    Update initiate_chat.php

--- src/lib/user/initiate_chat.php
+++ src/lib/user/initiate_chat.php
@@ -5,18 +5,15 @@ require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
 if (!isset($_SESSION['user'])) {
-    // Redirect if user is not logged in
     header('Location: /');
     exit();
 }
 
 if ($_SESSION["user"]["isTeacher"]) {
-    // Redirect teachers to a different page
     header('Location: https://bibliotheek.live');
     exit();
 }
 
-// Check if the necessary values are passed via GET
 if (isset($_GET['question']) && isset($_GET['img_code'])) {
     $question = $_GET['question'];
     $imgCode = $_GET['img_code'];
@@ -26,22 +23,19 @@ if (isset($_GET['question']) && isset($_GET['img_code'])) {
         ["type" => "s", "value" => $imgCode]
     );
 
-    // Insert into the Shoutbox table
     $query = 'INSERT INTO Shoutbox (Timestamp, Name, EMail, Text, Lobbyid, Senderid) VALUES (?, ?, ?, ?, ?)';
     insert(
         $query,
-        ['type' => 's', 'value' => ''.time().''], // Assuming 'Timestamp' column is of string type
+        ['type' => 's', 'value' => ''.time().''],
         ['type' => 's', 'value' => $_SESSION['user']['username']],
         ['type' => 's', 'value' => $question],
-        ['type' => 'i', 'value' => $lobbyTemp["id"]], // Assuming 'Lobbyid' column is of string type
-        ['type' => 'i', 'value' => $_SESSION['user']['id']], // Assuming 'Senderid' column is of integer type
+        ['type' => 'i', 'value' => $lobbyTemp["id"]], 
+        ['type' => 'i', 'value' => $_SESSION['user']['id']], 
     );
     
-    // Redirect after successful insertion
     header('Location: https://bibliotheek.live/alperenGit/src/public/user/chat/chat.php?code=' . $imgCode);
     exit();
 } else {
-    // Redirect if required values are not provided
     header('Location: https://bibliotheek.live');
     exit();
 }
