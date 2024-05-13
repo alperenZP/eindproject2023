46d7bd5d3df615f3455590340b8855c7199c8931
Aalleepphh <alperen.tok@bazandpoort.be>
2024-05-13 15:43:25 +0200

    Update create_test.php

--- src/public/user/teacher/create_test.php
+++ src/public/user/teacher/create_test.php
@@ -43,14 +43,6 @@ if (isset($_POST["addquestions"])){
 <div class="min-h-[100svh] w-full flex flex-col justify-center items-center p-8">
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8">Creëer nieuw toets</h1>
     <h1 class="sm:text-center md:text-center text-4xl font-bold mb-8"> </h1>
-    <form method="post" class="flex flex-col gap-8 w-full sm:w-80">
-                <div class="label">
-                    <span class="label-text">Hoeveel vragen?</span>
-                </div>
-                    <input type="number" name="aantal_vragen" placeholder="Hoeveel vragen?" class="input input-bordered" value="<?php echo $_SESSION["questions_amount"]?>" required />
-                    <button name="addquestions" class="btn btn-sm">Voeg vragen toe</button>
-                </div>
-    </form>
 
     <form method="post" class="flex flex-col gap-8 w-full sm:w-80">
         <div class="flex flex-col gap-4">
