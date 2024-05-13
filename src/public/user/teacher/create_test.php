4d1e158ab3a37e5690abaea9dfe8d48f9f556f14
Aalleepphh <alperen.tok@bazandpoort.be>
2024-05-13 15:10:17 +0200

    Update create_test.php

--- src/public/user/teacher/create_test.php
+++ src/public/user/teacher/create_test.php
@@ -52,10 +52,14 @@ $book = fetch(
                     <div class="card w-96 bg-base-100 shadow-xl">
                         <div class="card-body">
                             <h2 class="card-title">Card title!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div class="card-actions justify-end">
-                                <button class="btn btn-primary">Buy Now</button>
-                            </div>
+                            
+                            <ol type="A">
+                                <li><input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" /></li>
+                                <li><input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" /></li>
+                                <li><input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" /></li>
+                                <li><input type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" /></li>
+                            </ol>
+
                         </div>
                     </div>
                     ';
