90af2885fd1d7c2c21ae6b194f920432219afdb8
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-15 16:15:26 +0200

    Update create_test.php

--- src/lib/user/teacher/create_test.php
+++ src/lib/user/teacher/create_test.php
@@ -5,6 +5,21 @@ require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
 echo var_dump($_POST);
+
+for ($x = 0; $x < $_SESSION["questions_amount"]; $x++){
+    $correct = $x . 'correct';
+    $wrong1 = $x . 'wrong1';
+    $wrong2 = $x . 'wrong2';
+    $wrong3 = $x . 'wrong3';
+
+    echo '
+        <h1>Question '.$x.'</h1><br>
+        Correct: '.$_POST[$correct].'<br>
+        Wrong 1: '.$_POST[$wrong1].'<br>
+        Wrong 2: '.$_POST[$wrong2].'<br>
+        Wrong 3: '.$_POST[$wrong3].'<br>
+    ';
+}
 /*
 
 if (!isset($_SESSION['user'])) {
