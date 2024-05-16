e07e56b09e7325d14a9f075dc7ddfa755fb22888
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-16 18:20:42 +0200

    Update perform_test.php

--- src/public/user/student/perform_test.php
+++ src/public/user/student/perform_test.php
@@ -48,9 +48,9 @@ $questions = fetch_as_array('SELECT * FROM questions WHERE testid = ? ORDER BY i
                 $questions[$_SESSION["position_in_test"]]["incorrect3"],
             );
             shuffle($array_choices);
-
+            //https://bibliotheek.live/alperenGit/src/lib/user/student/perform_test.php
             echo '
-                <form action="https://bibliotheek.live/alperenGit/src/lib/user/student/perform_test.php" method="post" enctype="multipart/form-data" class="flex flex-col gap-8 w-full sm:w-80">
+                <form action="" method="post" enctype="multipart/form-data" class="flex flex-col gap-8 w-full sm:w-80">
                     <div class="flex flex-col gap-4">
                         <div class="mockup-window border bg-base-300">
                             <div class="flex flex-col px-4 py-8 bg-base-200"> <!-- Removed items-center -->
