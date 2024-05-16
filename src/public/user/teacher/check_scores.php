a7deda6e5fe0936dea306c2a260070f4640d7bff
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-16 22:38:43 +0200

    Update check_scores.php

--- src/public/user/teacher/check_scores.php
+++ src/public/user/teacher/check_scores.php
@@ -97,11 +97,9 @@ $questions_amount = $test_questions["questions_amount"];
 
                     $string = implode(", ", $score_array);
 
-                    if (empty($score_array)){
                         echo var_dump($score_array) . '<br>';
                         echo $test["id"] . '<br>';
                         echo $user["userid"] . '<br>';
-                    }
 
 
                     echo '
