16a95adb71126b19793c255bf8c79d07c142f9f9
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-16 20:27:35 +0200

    Update perform_test.php

--- src/lib/user/student/perform_test.php
+++ src/lib/user/student/perform_test.php
@@ -56,14 +56,9 @@ if (isset($_POST['submit'])) {
         $total = $total_amount_correct_query["amount"];
 
         header('Location: https://bibliotheek.live/alperenGit/src/public/user/student/perform_test.php?y='.$total.'&testid='.$testid);
+    } else {
+        header('Location: https://bibliotheek.live/alperenGit/src/public/user/student/perform_test.php?x=1&testid='.$testid);
     }
-
-
-    
-
-    
-    header('Location: https://bibliotheek.live/alperenGit/src/public/user/student/perform_test.php?x=1&testid='.$testid);
-
     exit();
 } 
 
