d7e317ef51d9c64c20bc7c6bc974530b44849128
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-16 20:16:46 +0200

    Update perform_test.php

--- src/lib/user/student/perform_test.php
+++ src/lib/user/student/perform_test.php
@@ -14,6 +14,7 @@ if (isset($_POST['submit'])) {
     $isAnswerCorrect = $_POST["result_question"];
     $question_id = $_POST["question_id"];
     $testid = $_POST["test_id"];
+    $amount_questions = $_POST["amount_of_questions"];
 
     $question = fetch(
         'SELECT * FROM questions WHERE id = ?',
@@ -21,14 +22,34 @@ if (isset($_POST['submit'])) {
     );
     $userid = $_SESSION["user"]["id"];
 
-    $query = 'INSERT INTO scores (questionid, userid, isCorrect) VALUES (?, ?, ?)';
-    insert(
-            $query,
-            ['type' => 'i', 'value' => $question_id],
-            ['type' => 'i', 'value' => $userid],
-            ['type' => 'i', 'value' => $isAnswerCorrect],
+    $check_if_question_already_answered = fetch(
+        'SELECT *,count(*) AS "amount" FROM scores WHERE questionid = ? AND userid = ?',
+        ["type" => "i", "value" => $question_id],
+        ["type" => "i", "value" => $userid]
     );
 
+    if ($check_if_question_already_answered["count"] > 0){
+        $query = 'UPDATE scores SET isCorrect = ? WHERE questionid = ? AND userid = ?';
+        insert(
+            $query,
+            ['type' => 'i', 'value' => $isAnswerCorrect],
+            ['type' => 'i', 'value' => $question_id],
+            ['type' => 'i', 'value' => $userid],
+        );
+    } else {
+        $query = 'INSERT INTO scores (questionid, userid, isCorrect) VALUES (?, ?, ?)';
+        insert(
+                $query,
+                ['type' => 'i', 'value' => $question_id],
+                ['type' => 'i', 'value' => $userid],
+                ['type' => 'i', 'value' => $isAnswerCorrect],
+        );
+    }
+
+
+    
+
+    
     header('Location: https://bibliotheek.live/alperenGit/src/public/user/student/perform_test.php?x=1&testid='.$testid);
 
     exit();
