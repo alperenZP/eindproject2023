d742c9232dc9d08e93f6260343d8edb95dbec666
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-24 16:47:03 +0200

    Update view_library.php

--- src/public/user/view_library.php
+++ src/public/user/view_library.php
@@ -88,7 +88,6 @@ $theme = 'dark';
                             <td>' . $book["description"] . '</td>
                             <td>
                                 <div class="badge badge-info gap-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
                                     In afwachting
                                 </div>
                             </td>
