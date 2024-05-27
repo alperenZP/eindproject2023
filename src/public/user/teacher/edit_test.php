26d3347589720616a51dc343e6b8f6349c32ff88
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-27 21:15:49 +0200

    Update edit_test.php

--- src/public/user/teacher/edit_test.php
+++ src/public/user/teacher/edit_test.php
@@ -76,7 +76,8 @@ if (isset($_GET["delq"])){
 
                 <?php
                 for ($y = 0; $y < count($questions); $y++) {
-                    $ynum = $y + 1;
+                    $oy = 'orig' + $y;
+                    $ynum =  $y + 1;
                     echo '
                     <div class="card w-96 bg-base-100 shadow-xl">
                         <div class="card-body">
@@ -87,12 +88,12 @@ if (isset($_GET["delq"])){
                     }
                     echo '
                             <h2 class="card-title">Vraag '.$ynum.'</h2>
-                            <h2 class="card-title"><input name="'.$y.'questiontext" type="text" value="'.$questions[$y]["text"].'" placeholder="Vraag '.$ynum.' titel" required class="input input-bordered input-md w-full max-w-xs" /></h2>
+                            <h2 class="card-title"><input name="'.$oy.'questiontext" type="text" value="'.$questions[$oy]["text"].'" placeholder="Vraag '.$ynum.' titel" required class="input input-bordered input-md w-full max-w-xs" /></h2>
                             <ol type="A">
-                                <b>Juist antwoord</b> <li><input name="'.$y.'correct" type="text" value="'.$questions[$y]["correct_option"].'" placeholder="Juist antwoord" required class="input input-bordered input-success input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 1</b> <li><input name="'.$y.'wrong1" type="text" value="'.$questions[$y]["incorrect1"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 2</b> <li><input name="'.$y.'wrong2" type="text" value="'.$questions[$y]["incorrect2"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 3</b> <li><input name="'.$y.'wrong3" type="text" value="'.$questions[$y]["incorrect3"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Juist antwoord</b> <li><input name="'.$oy.'correct" type="text" value="'.$questions[$oy]["correct_option"].'" placeholder="Juist antwoord" required class="input input-bordered input-success input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 1</b> <li><input name="'.$oy.'wrong1" type="text" value="'.$questions[$oy]["incorrect1"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 2</b> <li><input name="'.$oy.'wrong2" type="text" value="'.$questions[$oy]["incorrect2"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 3</b> <li><input name="'.$oy.'wrong3" type="text" value="'.$questions[$oy]["incorrect3"].'" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
                             </ol>
                         </div>
                     </div>
@@ -101,18 +102,17 @@ if (isset($_GET["delq"])){
 
                 for ($x = 0; $x < $_SESSION["questions_add_amount"]; $x++) {
                     $xnum = $x + count($questions) + 1;
-                    $xid = $x + count($questions);
                     echo '
                     <div class="card w-96 bg-base-100 shadow-xl">
                         <div class="card-body">
                             <a href="https://bibliotheek.live/alperenGit/src/public/user/teacher/edit_test.php?testid='.$test["id"].'&delq=1" class="exempt-link"><button type="button" class="btn btn-error">❌</button></a>
                             <h2 class="card-title">Vraag '.$xnum.'</h2>
-                            <h2 class="card-title"><input name="'.$xid.'questiontext" type="text" placeholder="Vraag '.$xnum.' titel" required class="input input-bordered input-md w-full max-w-xs" /></h2>
+                            <h2 class="card-title"><input name="'.$x.'questiontext" type="text" placeholder="Vraag '.$xnum.' titel" required class="input input-bordered input-md w-full max-w-xs" /></h2>
                             <ol type="A">
-                                <b>Juist antwoord</b> <li><input name="'.$xid.'correct" type="text" placeholder="Juist antwoord" required class="input input-bordered input-success input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 1</b> <li><input name="'.$xid.'wrong1" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 2</b> <li><input name="'.$xid.'wrong2" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
-                                <b>Fout antwoord 3</b> <li><input name="'.$xid.'wrong3" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Juist antwoord</b> <li><input name="'.$x.'correct" type="text" placeholder="Juist antwoord" required class="input input-bordered input-success input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 1</b> <li><input name="'.$x.'wrong1" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 2</b> <li><input name="'.$x.'wrong2" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
+                                <b>Fout antwoord 3</b> <li><input name="'.$x.'wrong3" type="text" placeholder="Fout antwoord" required class="input input-bordered input-error input-sm w-full max-w-xs" /></li>
                             </ol>
                         </div>
                     </div>
