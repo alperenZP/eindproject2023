72c7ab83f0af896f7c8d7a3c20094436d8f9940e
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-05-27 22:15:58 +0200

    Update delete_question.php

---
+++ src/lib/user/teacher/delete_question.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
+require_once DATABASE . '/connect.php';
+require_once LIB . '/util/util.php';
+
+if (!isset($_SESSION['user'])) {
+    header('Location: https://bibliotheek.live');
+    exit();
+}
+
+if (!$_SESSION["user"]["isTeacher"]) {
+    header('Location: https://bibliotheek.live');
+    exit();
+}
+
+if (isset($_GET['questionid'])) {
+    $question = fetch(
+        'SELECT * FROM `questions` WHERE id = ?',
+        ['type' => 'i', 'value' => $_GET['id']]
+    );
+
+    $testid = $question["testid"];
+
+    $query = 'DELETE FROM questions WHERE id = ?';
+    insert(
+        $query,
+        ['type' => 'i', 'value' => $_GET['questionid']],
+    );    
+
+    header('Location: https://bibliotheek.live/alperenGit/src/public/user/teacher/edit_test.php?testid=' . $testid . '');
+    exit();
+}
+
+header('Location: https://bibliotheek.live');
+exit();
