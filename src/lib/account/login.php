4ecba33f792689f5220bde4d0a84f653261f781c
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2024-06-01 15:49:07 +0200

    Update login.php

--- src/lib/account/login.php
+++ src/lib/account/login.php
@@ -1,10 +1,15 @@
 <?php
+session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/alperenGit/config.php';
 require_once DATABASE . '/connect.php';
 require_once LIB . '/util/util.php';
 
+if (isset($_SESSION['user'])) {
+  header('Location: /');
+  exit();
+}
+
 if (isset($_POST['login'])) {
-  session_start();
   login($_POST);
   return;
 }
